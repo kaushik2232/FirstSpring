66f1f1496323aa55ae79cb23fb3f06255db1130d
kaushik.satya77@gmail.com <kaushik.satya77@gmail.com>
2019-02-26 16:24:50 -0600

    Added Spring Bean Config Code

--- FirstSpring/src/main/java/com/Spring/Test/FirstSpring/App.java
+++ FirstSpring/src/main/java/com/Spring/Test/FirstSpring/App.java
@@ -1,11 +1,16 @@
 package com.Spring.Test.FirstSpring;
 
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 
 public class App 
 {
     public static void main( String[] args )
     {
-        Person person = new Person();
+    	ApplicationContext context = new FileSystemXmlApplicationContext("beans.xml");
+    	System.out.println("config loaded");
+        Person person = (Person)context.getBean("person");
         person.speak();
     }
 }
