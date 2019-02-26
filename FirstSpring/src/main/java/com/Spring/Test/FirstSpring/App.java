505071bce0e47548224690565046ce52af57ed86
kaushik.satya77@gmail.com <kaushik.satya77@gmail.com>
2019-02-25 20:55:48 -0600

    Added Person Class

---
+++ FirstSpring/src/main/java/com/Spring/Test/FirstSpring/App.java
@@ -0,0 +1,11 @@
+package com.Spring.Test.FirstSpring;
+
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Person person = new Person();
+        person.speak();
+    }
+}
